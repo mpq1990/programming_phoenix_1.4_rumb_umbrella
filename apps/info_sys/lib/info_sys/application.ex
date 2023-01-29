8164c90e6c0f2908fa4dbac24c0de0bbb51b69b8
Majid Qureshi <mpq1990@gmail.com>
2023-01-29 22:30:41 +0100

    feat: add info system

---
+++ apps/info_sys/lib/info_sys/application.ex
@@ -0,0 +1,23 @@
+defmodule InfoSys.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+alias Mix.Tasks.Hex.Info
+  @moduledoc false
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: InfoSys.Worker.start_link(arg)
+      # {InfoSys.Worker, arg}
+      InfoSys.Cache,
+      {Task.Supervisor, name: InfoSys.TaskSupervisor}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
